3c0ec0e213c3dca4e47d558de90b60245c0e5858
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-07-31 17:26:48 +0300

    Final commit.

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -23,7 +23,7 @@ export default function BlogPart() {
               </button> 
             </div>
             <div>
-              <h3>Trending Blog 2</h3>
+              <h3>Trading Different Time-Frames</h3>
               <p>The MetaTrader 4 (MT4) trading terminal is probably one of the most versatile trading and charting platforms combined. The MT4 platform offers a good amount of customization which traders can use to their benefit.
 
 While the MT4 platform offers a good selection of custom time frames, starting from 1-minute and up to 1-month, traders often find themselves in need of using custom time frames such as 2-hour, 6-hour, 8-hour or 12-hour charts.</p>
